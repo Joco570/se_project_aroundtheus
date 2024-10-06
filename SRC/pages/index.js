cf87c15dbca6efd4ff291875d3563132cdeb9ab3
Joe Coval <Joseph.m.coval@gmail.com>
2024-10-05 18:32:29 -0700

    constants foulder added and linked

--- SRC/pages/index.js
+++ SRC/pages/index.js
@@ -1,38 +1,8 @@
-// index.js
+import { initialCards, validationSettings } from "../utils/constants.js";
 import FormValidator from "../components/FormValidator.js";
 import Card from "../components/Card.js";
 import Section from "../components/Section.js";
 import "../pages/index.css";
-
-console.log("index.js loaded");
-
-// Data
-const initialCards = [
-  {
-    name: `Yosemite Valley`,
-    link: `https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg`,
-  },
-  {
-    name: `Lake Louise`,
-    link: `https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg`,
-  },
-  {
-    name: `Bald Mountains`,
-    link: `https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg`,
-  },
-  {
-    name: `Latemar`,
-    link: `https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg`,
-  },
-  {
-    name: `Vanoise National Park`,
-    link: `https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg`,
-  },
-  {
-    name: `Lago di Braies`,
-    link: `https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg`,
-  },
-];
 
 // Elements
 const profileEditButton = document.querySelector("#profile-edit-button");
@@ -49,16 +19,6 @@ const previewCaption = document.querySelector("#modal-caption");
 // Accessing forms using document.forms
 const profileForm = document.forms["profile-form"];
 const cardForm = document.forms["card-form"];
-
-// Validation settings
-const validationSettings = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__save",
-  inactiveButtonClass: "modal__save_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__input-error_visible",
-};
 
 // Object for storing form validators
 const formValidators = {};
@@ -80,8 +40,6 @@ const enableValidation = (config) => {
 
 // Initialize validation
 enableValidation(validationSettings);
-
-console.log("FormValidators initialized");
 
 // Function to render a card
 function renderCard(item) {
